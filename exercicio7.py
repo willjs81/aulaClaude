79a0f7beff53b1011a2d2d30dbe224a59d2dba30
wilson <jwill_js@outlook.com>
2026-03-08 09:45:30 -0300

    wip: iniciando exercicio7 - abrir arquivo cadastros

---
+++ exercicio7.py
@@ -0,0 +1,17 @@
+"""
+Exercício 7 — Leitura de arquivos
+
+Crie um arquivo exercicio7.py que:
+
+Leia o arquivo cadastros.txt gerado pelo exercício 6
+Exiba cada linha formatada no terminal
+Conte quantas pessoas estão cadastradas
+Se o arquivo não existir, exiba uma mensagem amigável em vez de quebrar o programa
+
+
+Dicas:
+
+Para ler arquivo use open("cadastros.txt", "r")
+Para tratar arquivo inexistente use try/except FileNotFoundError
+
+"""
